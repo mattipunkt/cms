50d7278c23ef32b5c8af539e65d85e00a4d90474
mattipunkt <mattihro105@gmail.com>
2025-12-17 10:10:17 +0100

    only get upcoming showtimes at movie details

--- routes/web.php
+++ routes/web.php
@@ -80,7 +80,7 @@ Route::get('/api/movie/{id}/showtimes', function ($id, Request $request) {
             ->with(['location', 'movie', 'event'])
             ->get());
     }
-    return ShowtimeResource::collection(Showtime::with(['location', 'event'])->where('movie_id', $id)->get());
+    return ShowtimeResource::collection(Showtime::with(['location', 'event'])->where('movie_id', $id)->where('time', '>', Carbon::now())->get());
 });
 Route::get('/api/today', function () {
     return ShowtimeResource::collection(Showtime::with(['location', 'movie', 'event'])->today()->get());
